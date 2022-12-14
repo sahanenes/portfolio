106b71ff091de361be8b158b38ec3891e804ccb9
sahanenes <sahanenesss@gmail.com>
2022-12-14 16:08:57 +0300

    contact added

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import "./testimonials.css";
+// import React from "react";
+// import "./testimonials.css";
 
-const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
-};
+// const Testimonials = () => {
+//   return (
+//     <section id="testimonials">
+//       <h5>Reviews</h5>
+//       <h2>Testimonials</h2>
+//       <div className="container testimonials__container">
+//         <article className="testimonial"></article>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Testimonials;
+// export default Testimonials;
